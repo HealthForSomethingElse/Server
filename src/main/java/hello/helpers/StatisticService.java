40a700ccba9d629b04bc12f5a23e0fdd649f9ff9
Leshka <31736716+TOPlessss@users.noreply.github.com>
2019-12-22 22:01:01 +0300

    db

--- src/main/java/hello/helpers/StatisticService.java
+++ src/main/java/hello/helpers/StatisticService.java
@@ -31,7 +31,7 @@ public class StatisticService {
         User userInDb = userRepository.findUserById(statisticFromClient.getUserId());
         String message;
         Integer status;
-        if(statisticInDb.getUserId() != null){
+        if(statisticInDb != null){
             statisticRepository.save(statisticFromClient);
             message = "Statistic saved";
             System.out.println("statistic saved" + inputJson);
