e40c223f1cc7d78b121fcf36a161da27e720f116
Leshka <31736716+TOPlessss@users.noreply.github.com>
2019-12-24 14:11:51 +0300

    cal

--- src/main/java/hello/repos/StatisticRepository.java
+++ src/main/java/hello/repos/StatisticRepository.java
@@ -5,11 +5,12 @@ import hello.domain.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Calendar;
 import java.util.Date;
 
 @Repository
 public interface StatisticRepository extends CrudRepository<Statistic, Integer> {
-    boolean existsStatisticByUserIdAndDate(Integer id, Date date);
-    Statistic findByUserIdAndDate(Integer id, Date date);
+    boolean existsStatisticByUserIdAndDate(Integer id, Calendar date);
+    Statistic findByUserIdAndDate(Integer id, Calendar date);
 
 }
